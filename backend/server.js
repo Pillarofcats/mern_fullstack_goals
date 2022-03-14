3c019c6d199682c918571c0de5ac4b983ade75c5
Pillarofcats <jacob.white.07@gmail.com>
2022-03-13 19:16:15 -0500

    Fixing deployment issue

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
 const path = require('path')
-const fs = require('fs')
+
 //Import custom middleware
 const  { errorHandler } = require('./middlware/errorMiddlware.js')
 //Import mongoDB connection
